f3cd9b0128ea101ad7083fde4fd4daec715d7f05
waiwai1000 <41092236+waiwai1000@users.noreply.github.com>
2019-10-25 15:32:30 +0800

    convert pop up video to sound only video (issue: after using the extension , all youtube video become sound only)

--- overlay/overlay_yt.js
+++ overlay/overlay_yt.js
@@ -1,4 +1,11 @@
 function checkForEmbeds() {
+  chrome.webRequest.onBeforeRequest.addListener(
+    processRequest,
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+  );
+
+
   if(removeIcons)
     return;
   if (!(host.indexOf('youtube.com') > -1)) return;
@@ -92,3 +99,54 @@ function ytWatchElementHandler(el) {
   });
   el.appendChild(tmp);
 }
+
+chrome.runtime.sendMessage('enable-youtube-audio');
+
+var makeSetAudioURL = function(videoElement, url) {
+   
+    if (videoElement.src != url) {
+		var paused = videoElement.paused;
+        videoElement.src = url;
+		if (paused === false) {
+			videoElement.play();
+		}
+    }
+};
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        let url = request.url;
+        
+        let videoElement = document.getElementsByTagName('video')[0];
+        //  console.log(document.getElementsByTagName('video')[0]);
+		videoElement.onloadeddata = makeSetAudioURL(videoElement, url);
+
+
+        // let audioOnlyDivs = document.getElementsByClassName('audio_only_div');
+        // Append alert text
+        // if (audioOnlyDivs.length == 0 && url.includes('mime=audio')) {
+        //     // let extensionAlert = document.createElement('div');
+        //     // extensionAlert.className = 'audio_only_div';
+
+        //     // let alertText = document.createElement('p');
+        //     // alertText.className = 'alert_text';
+        //     // alertText.innerHTML = 'Sound only.';
+
+        //     // extensionAlert.appendChild(alertText);
+        //     // let parent = videoElement.parentNode.parentNode;
+
+        //     // Append alert only if options specify to do so
+        //     // chrome.storage.local.get('disable_video_text', function(values) {
+        //     //   var disableVideoText = (values.disable_video_text ? true : false);
+        //     //   if (!disableVideoText && parent.getElementsByClassName("audio_only_div").length == 0)
+        //     //     parent.appendChild(extensionAlert);
+        //     // });
+        // }
+        // else if (url == "") {
+        //     // for(div in audioOnlyDivs) {
+        //     //     div.parentNode.removeChild(div);
+        //     // }
+        // }
+    }
+);
+
